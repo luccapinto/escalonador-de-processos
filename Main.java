05394a65908af7b284a6f943837b4342f0d1a4d6
Lucca Rodrigues Assunção Pinto <tripaca123@gmail.com>
2023-10-15 01:37:52 -0300

    nao aguento maiskkkkkkkkkkk

--- Main.java
+++ Main.java
@@ -1,60 +1,93 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.LinkedList;
-import java.util.Queue;
 
 public class Main {
     public static void main(String[] args) {
         String programsPath = "programas";
         LogFile logFile = new LogFile("log.txt");
 
+        // Carregar os programas
+        List<String> programs = loadPrograms(programsPath, logFile);
+        
         int quantum = readQuantum("programas/quantum.txt");
         if (quantum <= 0) {
             System.out.println("Quantum inválido. Finalizando programa.");
             return;
         }
-        
-        // Criando a fila de BCPs
-        Queue<BCP> readyprocesses = new LinkedList<>();
-        Queue<BCP> blockedprocesses = new LinkedList<>();
 
-        // Carregando os BCPs
+        int totalSwitches = 0;
+        int totalInstructions = 0;
+
+        while (!programs.isEmpty()) {
+            List<String> finishedPrograms = new ArrayList<>();
+            for (String program : programs) {
+                logFile.writeLog("Executando " + program);
+                
+                int instructionsExecuted = runProgram(program, quantum);
+                totalInstructions += instructionsExecuted;
+                
+                if (isProgramFinished(program)) {
+                    logFile.writeLog(program + " terminado. X=x_value. Y=y_value");
+                    finishedPrograms.add(program);
+                } else {
+                    logFile.writeLog("Interrompendo " + program + " após " + instructionsExecuted + " instruções");
+                    totalSwitches++;
+                }
+            }
+            programs.removeAll(finishedPrograms);
+        }
+        
+        logFile.writeLog("MEDIA DE TROCAS: " + totalSwitches);
+        if(totalSwitches != 0) {
+            logFile.writeLog("MEDIA DE INSTRUCOES: " + (double) totalInstructions / totalSwitches);
+        } else {
+            logFile.writeLog("MEDIA DE INSTRUCOES: NaN");
+        }
+        logFile.writeLog("QUANTUM: " + quantum);
+        
+        logFile.readLog();
+    }
+
+    private static List<String> loadPrograms(String path, LogFile logFile) {
+        List<String> programs = new ArrayList<>();
         try {
-            Files.list(Paths.get(programsPath))
-                    .filter(Files::isRegularFile)
-                    .map(Path::toFile)
-                    .forEach(file -> {
-                        try {
-                            BCP bcp = new BCP(file.getPath());
-                            readyprocesses.add(bcp);
-                            logFile.writeLog("Carregando " + bcp.getName());
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                            // Lide com o erro conforme necessário
-                        }
-                    });
+            Files.list(Paths.get(path))
+                 .filter(Files::isRegularFile)
+                 .map(Path::toFile)
+                 .forEach(file -> {
+                     logFile.writeLog("Carregando " + file.getName());
+                     programs.add(file.getName().replace(".txt", ""));
+                 });
         } catch (IOException e) {
             e.printStackTrace();
             // Trate o erro conforme a necessidade do seu aplicativo.
         }
-
-        // Instanciando e iniciando o Scheduler
-        Scheduler scheduler = new Scheduler(readyprocesses, quantum);
-        scheduler.executeProcesses();
-        
-        // Logando informações finais
-        logFile.writeLog("QUANTUM: " + quantum);
-        logFile.readLog();
+        return programs;
+    }
+    
+    private static int runProgram(String program, int quantum) {
+        // Mocked execution logic. Replace with actual logic for running programs.
+        int instructionsExecuted = (int)(Math.random() * quantum) + 1; // Random number from 1 to quantum
+        return instructionsExecuted;
+    }
+    
+    private static boolean isProgramFinished(String program) {
+        // Mocked finish check logic. Replace with actual logic for checking program finish state.
+        return Math.random() > 0.5; // Randomly finishes or not
     }
 
     private static int readQuantum(String path) {
-        try (var br = Files.newBufferedReader(Path.of(path))) {
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
             return Integer.parseInt(br.readLine().trim());
         } catch (IOException | NumberFormatException e) {
             e.printStackTrace();
-            return -1;
+            return -1;  
         }
     }
 }
--- Scheduler.java
+++ Scheduler.java
@@ -1,3 +1,4 @@
+import java.util.LinkedList;
 import java.util.Queue;
 
 public class Scheduler {
@@ -7,54 +8,47 @@ public class Scheduler {
 
     public Scheduler(Queue<BCP> processes, int quantum) {
         this.readyprocesses = processes;
+        this.blockedprocesses = new LinkedList<>(); // Inicialização da fila de processos bloqueados
         this.quantum = quantum;
     }
 
-public void executeProcesses() {
-    while (!readyprocesses.isEmpty() || !blockedprocesses.isEmpty()) {
-        // Selecionar próximo BCP a ser executado na readyQueue...
-        BCP bcp = readyprocesses.poll();
+    public void executeProcesses() {
+        while (!readyprocesses.isEmpty() || !blockedprocesses.isEmpty()) {
+            // Manipulação de processos bloqueados...
+            // [Lógica para mover processos de blockedprocesses para readyprocesses conforme necessário]
 
-        // Verificando se o BCP não é nulo (por segurança)
-        if (bcp != null) {
-            // Definir estado como executando
-            bcp.setState("EXECUTANDO");
-            logProcessAction("Executando " + bcp.getName());
+            BCP bcp = readyprocesses.poll();
+            if (bcp != null) {
+                bcp.setState("EXECUTANDO");
+                logProcessAction("Executando " + bcp.getName());
 
-            // Exemplo: Execute até 'quantum' instruções ou até o processo bloquear/terminar
-            int executedInstructions = executeInstructions(bcp, quantum);
-
-            // Verifica o estado após a execução de instruções
-            switch (bcp.getState()) {
-                case "BLOQUEADO":
-                    logProcessAction(bcp.getName() + " bloqueado após " + executedInstructions + " instruções.");
-                    blockedprocesses.add(bcp);
-                    break;
-                case "TERMINADO":
-                    logProcessAction(bcp.getName() + " terminado. X=" + bcp.getX() + ". Y=" + bcp.getY());
-                    break;
-                case "EXECUTANDO":
-                    logProcessAction("Interrompendo " + bcp.getName() + " após " + executedInstructions + " instruções.");
-                    readyprocesses.add(bcp); // Retorna para a fila de prontos para continuar execução posteriormente
-                    break;
+                int executedInstructions = executeInstructions(bcp, quantum);
+                
+                switch (bcp.getState()) {
+                    case "BLOQUEADO":
+                        logProcessAction(bcp.getName() + " bloqueado após " + executedInstructions + " instruções.");
+                        blockedprocesses.add(bcp);
+                        break;
+                    case "TERMINADO":
+                        logProcessAction(bcp.getName() + " terminado. X=" + bcp.getX() + ". Y=" + bcp.getY());
+                        break;
+                    case "EXECUTANDO":
+                        logProcessAction("Interrompendo " + bcp.getName() + " após " + executedInstructions + " instruções.");
+                        readyprocesses.add(bcp);
+                        break;
+                }
             }
         }
-
-        // ... [Demais lógicas, como lidar com processos bloqueados, etc.]
     }
-}
 
     private int executeInstructions(BCP bcp, int quantum) {
-    int executedInstructions = 0;
-
-    // Sua lógica para execução das instruções aqui. 
-    // Atualizar os estados de 'bcp' conforme necessário.
-
-    return executedInstructions;
-}
+        int executedInstructions = 0;
+        // Implemente a lógica de execução de instruções aqui, modificando bcp conforme necessário.
+        return executedInstructions;
+    }
 
     public void logProcessAction(String actionDetails) {
         System.out.println(actionDetails);
-        // TODO: Implement logging to a file or other destinations if needed...
+        // Implemente o logging para um arquivo ou outros destinos se necessário.
     }
 }
