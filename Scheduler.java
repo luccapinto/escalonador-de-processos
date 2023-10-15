83eee97c50553659214d88cd8017d8320d575b7d
Lucca Rodrigues Assunção Pinto <tripaca123@gmail.com>
2023-10-15 02:51:11 -0300

    Scheduler pronto!
    Problema pra entrar na função de escalonamento

--- Scheduler.java
+++ Scheduler.java
@@ -22,7 +22,7 @@ public class Scheduler {
             // Manipulação de processos bloqueados...
             // [Lógica para mover processos de blockedprocesses para readyprocesses conforme necessário]
             if(!blockedprocesses.isEmpty()){
-                for(int n = 0; i < blockedprocesses.size(); n++){
+                for(int n = 0; n < blockedprocesses.size(); n++){
                     blockedprocesses.get(n).decreaseWaitTime();
                         if(blockedprocesses.get(n).getWaitTime() <= 0){
                             readyprocesses.add((blockedprocesses).get(n));
@@ -34,8 +34,8 @@ public class Scheduler {
             if (bcp != null) {
                 bcp.setState("EXECUTANDO");
                 logFile.writeLog("Executando " + bcp.getName());
-
-                int executedInstructions = executeInstructions(bcp, quantum);
+                int pcinicial = bcp.getPc();
+                int executedInstructions = executeInstructions(bcp, quantum, i);
                 switch (bcp.getState()) {
                     case "BLOQUEADO":
                         logFile.writeLog(bcp.getName() + " bloqueado após " + executedInstructions + " instruções.");
@@ -45,42 +45,46 @@ public class Scheduler {
                         logFile.writeLog(bcp.getName() + " terminado. X=" + bcp.getX() + ". Y=" + bcp.getY());
                         break;
                     case "EXECUTANDO":
-                        logFile.writeLog("Interrompendo " + bcp.getName() + " após " + executedInstructions + " instruções.");
-                        readyprocesses.add(bcp);
+                        if(bcp.getPc() - pcinicial == 0){
+                            bcp.setState("TERMINADO");
+                            readyprocesses.remove(i);
+                        }
+                        else{
+                            logFile.writeLog("Interrompendo " + bcp.getName() + " após " + executedInstructions + " instruções.");
+                            readyprocesses.add(bcp);
+                        }
                         break;
                 }
+                i++;
             }
             }
         }
 
     }
 
-    private int executeInstructions(BCP bcp, int quantum) {
+    private int executeInstructions(BCP bcp, int quantum, int index) {
         int executedInstructions = 0;
-        for(int j = 0; readyprocesses.get(0).getPc() < readyprocesses.get(0).getInstructions().size() && j < quantum; readyprocesses.get(0).incrementPc(), j++){
-            String comando = readyprocesses.get(0).getInstructions().get(readyprocesses.get(0).getPc()).substring(0, 2); 
+        for(int j = 0; readyprocesses.get(index).getPc() < readyprocesses.get(index).getInstructions().size() && j < quantum; readyprocesses.get(index).incrementPc(), j++){
+            String comando = readyprocesses.get(index).getInstructions().get(readyprocesses.get(index).getPc()).substring(0, 2); 
             switch(comando){
                 case "CO":
                 executedInstructions++;
                 break;
                 case "E/":
-                    readyprocesses.get(0).setWaitTime(2);
-                    blockedprocesses.add(readyprocesses.get(0));
+                    readyprocesses.get(index).setWaitTime(2);
+                    blockedprocesses.add(readyprocesses.get(index));
+                    readyprocesses.remove(index);
                     bcp.setState("BLOQUEADO");
                     j=quantum; //força saída do loop
                     executedInstructions++;
                 break;
                 case "SA":
                     bcp.setState("TERMINADO");
+                    readyprocesses.remove(index);
                     j=quantum; //força saída do loop
                 break;
             }
         }
         return executedInstructions;
-    }
-
-    public void logProcessAction(String actionDetails) {
-        System.out.println(actionDetails);
-        // Implemente o logging para um arquivo ou outros destinos se necessário.
     }
 }
