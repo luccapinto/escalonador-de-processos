25136e7509885696b824e2e32d02d567dc62cc57
Lucca Rodrigues Assunção Pinto <tripaca123@gmail.com>
2023-10-22 01:03:09 -0300

    Arrumando o warning de writer não fechado

--- LogFile.java
+++ LogFile.java
@@ -1,23 +1,31 @@
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.BufferedReader;
-import java.io.FileReader;
 
 public class LogFile {
     private String filePath;
 
-    // Constructor
     public LogFile(String filePath) {
         this.filePath = filePath;
+        BufferedWriter writer = null;
         try {
-            BufferedWriter writer = new BufferedWriter(new FileWriter(filePath));
+            writer = new BufferedWriter(new FileWriter(filePath));
+            // File is created
         } catch (IOException e) {
             e.printStackTrace();
+        } finally {
+            if (writer != null) {
+                try {
+                    writer.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
+    
 
-    // Method to write a log message to the file
+    // Escritor de logs
     public void writeLog(String message) {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath, true))) { // true for append mode
             writer.write(message);
@@ -26,19 +34,6 @@ public class LogFile {
             System.err.println("Error writing the log message: " + e.getMessage());
         }
     }
-
-    // Method to read the log file
-    public void readLog() {
-        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
-            String line = null;
-            while ((line = reader.readLine()) != null) {
-                System.out.println(line);
-            }
-        } catch (IOException e) {
-            System.err.println("Error reading the log file: " + e.getMessage());
-        }
-    }
-
     // Getters and Setters
     public String getFilePath() {
         return filePath;
