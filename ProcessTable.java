131f324b50dd5e697152d9cfedacf9f364245349
Lucca Rodrigues Assunção Pinto <tripaca123@gmail.com>
2023-10-22 02:37:37 -0300

    Adicionando comentários

--- ProcessTable.java
+++ ProcessTable.java
@@ -4,26 +4,26 @@ import java.util.List;
 public class ProcessTable {
     private List<BCP> processList;
 
-    public ProcessTable() {
+    public ProcessTable() { // Cria a tabela de processos, vamos usar lista para isso
         processList = new ArrayList<>();
     }
     
-    public void newList(List<BCP> programs){
+    public void newList(List<BCP> programs){ // Importa a lista de programas para a tabela de processos
         processList = programs;
     }
     
-    public void addProcess(BCP bcp) {
+    public void addProcess(BCP bcp) { // Adiciona um processo à lista
         processList.add(bcp);
     }
 
-    public BCP get(int index){
+    public BCP get(int index){ // Pega o processo que está em determinada posição da lista
         return processList.get(index);
     }
 
-    public void removeProcess(BCP bcp) {
+    public void removeProcess(BCP bcp) { // Remove o processo da lista
         processList.remove(bcp);
     }
-    public int size(){
+    public int size(){ 
         return processList.size();
     }
 
